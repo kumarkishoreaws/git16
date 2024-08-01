c9ba994612efb36addef33c346900c5566d8e559
kumarkishoreaws <kichuu555555@gmail.com>
2024-08-01 16:30:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const PORT = process.env.PORT || 5000;
 const app = express();
 require("./db");
-const productRoute = require("./routes/product)
+const productRoute = require("./routes/product
 const authRoute = require("./routes/auth");
 const path = require("path");
 /************ MIDDLEVARES *************/
